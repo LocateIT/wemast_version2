49c9cc1660fc893847c62e3014a0774710fe260c
unknown <mutindarisper@gmail.com>
2023-01-26 11:57:14 +0300

    implemented a spinner while the wms loads

--- src/App.vue
+++ src/App.vue
@@ -280,6 +280,13 @@
 
         </div>
 
+
+
+        <div  class="spinner"  v-if="loading">
+            <img src="../assets/loader_white.svg" alt="">
+        </div>
+             
+
   </div>
   
   
@@ -299,6 +306,7 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw-src.css";
 import "leaflet-draw";
 import "leaflet.wms"
+import $ from "jquery";
 // import "leaflet-draw/dist/leaflet.draw";
 
 //trial custom shapefile upload
@@ -354,6 +362,7 @@ let indicator = ref(null)
 let sub_indicator = ref(null)
 let year = ref(null)
 let styles = ref(null)
+
 
 
 
@@ -936,9 +945,102 @@ watch( setSelectedYear , () => {
 })
 
 
+
+L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
+  
+  onAdd: function (map) {
+    // Triggered when the layer is added to a map.
+    //   Register a click listener, then do all the upstream WMS things
+    L.TileLayer.WMS.prototype.onAdd.call(this, map);
+    map.on('click', console.log('better wms function click') , this.getFeatureInfo, this);
+  },
+  
+  onRemove: function (map) {
+    // Triggered when the layer is removed from a map.
+    //   Unregister a click listener, then do all the upstream WMS things
+    L.TileLayer.WMS.prototype.onRemove.call(this, map);
+    map.off('click', this.getFeatureInfo, this);
+  },
+  
+  getFeatureInfo: function (evt) {
+    // Make an AJAX request to the server and hope for the best
+    var url = this.getFeatureInfoUrl(evt.latlng),
+        showResults = L.Util.bind(this.showGetFeatureInfo, this);
+    $.ajax({
+      url: url,
+      success: function (data, status, xhr) {
+        var err = typeof data === 'string' ? null : data;
+        showResults(err, evt.latlng, data);
+      },
+      error: function (xhr, status, error) {
+        showResults(error);  
+      }
+    });
+  },
+  
+  getFeatureInfoUrl: function (latlng) {
+    // Construct a GetFeatureInfo request URL given a point
+    var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
+        size = this._map.getSize(),
+        
+        params = {
+          request: 'GetFeatureInfo',
+          service: 'WMS',
+          srs: 'EPSG:4326',
+          styles: this.wmsParams.styles,
+          transparent: this.wmsParams.transparent,
+          version: this.wmsParams.version,      
+          format: this.wmsParams.format,
+          bbox: this._map.getBounds().toBBoxString(),
+          height: size.y,
+          width: size.x,
+          layers: this.wmsParams.layers,
+          query_layers: this.wmsParams.layers,
+          info_format: 'application/json'
+        };
+    
+    params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+    params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+    
+    return this._url + L.Util.getParamString(params, this._url, true);
+  },
+  
+  showGetFeatureInfo: function (err, latlng, content) {
+    if (err) {
+      // console.log(latlng, 'lat long')
+      
+      
+
+      console.log(content, 'wms content')
+    
+
+
+        return  
+        // console.log(latlng, 'lat long');
+      
+      } // do nothing if there's an error
+   
+  
+    // Otherwise show the content in a popup, or something.
+    // L.popup({ maxWidth: 800})
+    //   .setLatLng(latlng)
+    //   .setContent(content)
+    //   .openOn(this._map);
+  }
+}); //end of L.extend
+
+
+
+L.tileLayer.betterWms = function (url, options) {
+  return new L.TileLayer.BetterWMS(url, options);  
+};
+
+
+
 //function to request wms
 
 const fetchWmsData = () => {
+  loading.value = true
   if(wmsLayer.value)map.removeLayer(wmsLayer.value)
 
 
@@ -955,11 +1057,16 @@ const fetchWmsData = () => {
   if(basin.value === 'Okavango' && sub_indicator.value === 'Land Cover'){
     styles.value = 'okavango_lulc'
   }
+
+
+
+
+ 
   
   // console.log('just to see if request is accessed') //accessed
   map.createPane("pane800").style.zIndex = 2500;
 
-wmsLayer.value =  L.tileLayer.wms("http://66.42.65.87:8080/geoserver/WEMAST/wms?", {
+wmsLayer.value =  L.tileLayer.betterWms("http://66.42.65.87:8080/geoserver/WEMAST/wms?", {
       pane: 'pane800',
       layers: 'LULC',
       crs:L.CRS.EPSG4326,
@@ -972,6 +1079,11 @@ wmsLayer.value =  L.tileLayer.wms("http://66.42.65.87:8080/geoserver/WEMAST/wms?
 });
 
 wmsLayer.value.addTo(map);
+//remove spinner when layer loads
+wmsLayer.value.on('load', function (event) {
+     loading.value = false
+});
+
 
 }
 

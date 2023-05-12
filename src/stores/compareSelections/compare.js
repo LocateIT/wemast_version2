ce6f5a70bef969cf9f0d7ac0d73f435e6d0347e2
unknown <mutindarisper@gmail.com>
2023-05-12 17:04:25 +0300

    added stats for prec index wet compare

--- src/stores/compareSelections/compare.js
+++ src/stores/compareSelections/compare.js
@@ -333,63 +333,63 @@ export const useCompareStore = defineStore({
             }
           
           }
-        //   if(this.selected_sub_indicator === 'Precipitation Index' && this.selected_season === 'WET') {
-        //     try {
+          if(this.selected_sub_indicator === 'Precipitation Index' && storeUserSelections.selected_season === 'WET') {
+            try {
             
-        //       // console.log(this.selected_basin, 'BASIN FOR STATISTICS')
-        //    var basin = this.selected_basin
-        //   //  console.log(basin, 'ddaaaaaaaaattttttttttttttaaaaaaaaaaa')
+              // console.log(this.selected_basin, 'BASIN FOR STATISTICS')
+          //  var basin = this.selected_basin
+          //  console.log(basin, 'ddaaaaaaaaattttttttttttttaaaaaaaaaaa')
+          var basin = storeUserSelections.selected_basin
     
-    
-        //    var year = this.selected_year
-        //   //  console.log(year, 'year FOR SSSSTAAAAAAAAAAAATTTTTTTTTTS')
-        //   var season = this.selected_season
+           var year = this.selected_year
+          //  console.log(year, 'year FOR SSSSTAAAAAAAAAAAATTTTTTTTTTS')
+          // var season = this.selected_season
   
-        //   // console.log(season, 'season for stattttttttsssssss')
+          // console.log(season, 'season for stattttttttsssssss')
         
       
-        //       const response = await axios.get(`http://66.42.65.87:8080/geoserver/SPI_MAX/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=SPI_MAX%3A${year}.shp&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=name=%27${basin}%27`
-        //       );
-        //       console.log(response.data.features[0].properties,'stats response')
-        //       var obj = response.data.features[0].properties
+              const response = await axios.get(`http://66.42.65.87:8080/geoserver/SPI_MAX/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=SPI_MAX%3A${year}.shp&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=name=%27${basin}%27`
+              );
+              console.log(response.data.features[0].properties,'stats response')
+              var obj = response.data.features[0].properties
               
-        //       const newObj = Object.fromEntries(Object.entries(obj).filter(([key]) => !key.includes('id') && !key.includes('geom') && !key.includes('name') && !key.includes('0')&& !key.includes('count')))
-        //       console.log(newObj, 'NEW OBJECT')
+              const newObj = Object.fromEntries(Object.entries(obj).filter(([key]) => !key.includes('id') && !key.includes('geom') && !key.includes('name') && !key.includes('0')&& !key.includes('count')))
+              console.log(newObj, 'NEW OBJECT')
       
-        //       var labels = Object.keys(newObj)
-        //       console.log(labels, 'stats labels')
-        //       this.lulcChartData.labels = labels
+              var labels = Object.keys(newObj)
+              console.log(labels, 'stats labels')
+              this.lulcChartData.labels = labels
              
             
-        //       var figures = Object.values(newObj)
-        //       console.log(figures, 'stats figures')
-        //       // var converted = figures.map( (item) => item/100)
-        //       // console.log(converted, 'converted figres')
-        //       this.lulcChartData.datasets[0].data = figures
-        //       this.lulcChartData.datasets[0].backgroundColor = ['#6aff4e', '#13a147',  '#93be89']
+              var figures = Object.values(newObj)
+              console.log(figures, 'stats figures')
+              // var converted = figures.map( (item) => item/100)
+              // console.log(converted, 'converted figres')
+              this.lulcChartData.datasets[0].data = figures
+              this.lulcChartData.datasets[0].backgroundColor = ['#6aff4e', '#13a147',  '#93be89']
   
   
-        //        //for new array
-        //        this.stats_array.labels = labels
-        //        this.stats_array.data_figures = figures
+               //for new array
+               this.stats_array.labels = labels
+               this.stats_array.data_figures = figures
   
-        //        //capture bbox
-        //        var bbox = response.data.features[0].bbox
-        //        console.log(bbox, 'BOUNDING BOX')
-        //         this.western_lon = bbox[0]
-        //         this.northern_lat = bbox[1]
-        //         this.eastern_lon = bbox[2]
-        //         this.southern_lat = bbox[3]
-        //         this.resolution = '300'
+               //capture bbox
+               var bbox = response.data.features[0].bbox
+               console.log(bbox, 'BOUNDING BOX')
+                this.western_lon = bbox[0]
+                this.northern_lat = bbox[1]
+                this.eastern_lon = bbox[2]
+                this.southern_lat = bbox[3]
+                this.resolution = '300'
               
            
               
-        //     } catch (error) {
-        //       console.error('an error occured'+error);
+            } catch (error) {
+              console.error('an error occured'+error);
               
-        //     }
+            }
           
-        //   }
+          }
   
   
   
